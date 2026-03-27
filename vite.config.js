cfe9f4f71bfdb5db16fb41c5ac161bfa69cd1eaf
༺ 𒆜ASC𒆜 ༻ <murodovshohruh2003@gmail.com>
2026-03-27 13:45:58 +0500

    Remove viteStaticCopy plugin from vite.config.js
    Removed viteStaticCopy plugin configuration from Vite config.

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  base: '/MurodovShohruh/',  // 👈 GitHub Pages repo nomiga mos
+  base: '/MurodovShohruh/',
   plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        { src: 'index.html', dest: '' }, // React Router uchun 404 muammosi
-      ]
-    })
+    react()
   ],
   build: {
     outDir: 'dist'
