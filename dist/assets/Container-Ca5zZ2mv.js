8005b9f3ba5f425e9e460a8845da961407472bbe
༺ 𒆜ASC𒆜 ༻ <murodovshohruh2003@gmail.com>
2026-03-27 13:40:10 +0500

    Delete dist directory

--- dist/assets/Container-Ca5zZ2mv.js
+++
@@ -1,1 +0,0 @@
-import{j as x}from"./index-TwWwtYPv.js";function n({children:t,className:o="",id:s}){return x.jsx("section",{id:s,className:`max-w-6xl mx-auto px-6 py-24 ${o}`,children:t})}export{n as C};
